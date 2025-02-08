cd3d0a3a5de1ef297a5e08ddafd4596dbb74e5d1
Michael Powers <mgrpowers@gmail.com>
2025-02-08 13:43:59 -0800

    updated copy

--- wagpow/src/components/Schedule.tsx
+++ wagpow/src/components/Schedule.tsx
@@ -12,8 +12,15 @@ export const Schedule = () => {
 			<h2>Saturday, August 9, 2025, evening</h2>
 			<p>Wedding</p>
 			<p>
-				Please join us at General Prim for our wedding ceremony. Drinks,
-				dinner, and celebration to follow.
+				Please join us at{" "}
+				<a
+					target="_blank"
+					href="https://www.google.com/maps/place/Proyectos+P%C3%BAblicos/@19.4294481,-99.1527793,17z/data=!3m1!4b1!4m6!3m5!1s0x85d1ff2d1ad218f3:0x91c0786937fd6dc8!8m2!3d19.4294481!4d-99.1527793!16s%2Fg%2F11c0vlh4jj?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D"
+				>
+					General Prim
+				</a>{" "}
+				for our wedding ceremony. Drinks, dinner, and celebration to
+				follow.
 			</p>
 			<p>Dress code: Cocktail/Formal</p>
 			<h2>Sunday, August 10, 2025</h2>
--- wagpow/src/components/TravelInformation.tsx
+++ wagpow/src/components/TravelInformation.tsx
@@ -1,3 +1,22 @@
+// "Passport
+// Mexico requires that your passport is valid upon entry to the country. For additional information, please visit the Mexican government's website.
+
+// Travel to Mexico City
+// We suggest flying into Mexico City's main airport, Mexico City International Airport Benito Juárez (airport code MEX). The other airports that may show up when searching for flights are far outside the city.
+
+// Travel within Mexico City
+// We suggest using ridesharing applications (we use Uber) to travel from the airport to your accommodations. We DO NOT recommend renting a car, as the roads can be difficult and parking is complicated. Once you are at your accommodations, Mexico City is walkable, public transit is reliable, and ridesharing cars are plentiful.
+
+// Accommodations
+// We lived in the Condesa neighborhood when we lived in Mexico City, and we recommend our guests stay in either the Condesa or Roma neighborhoods. There are lots of lovely smaller hotels in the neighborhood that have a lot of character. Please ask us if you have questions about specific hotels or neighborhoods. Please check back for more information.
+
+// Other Information
+// Mexico City is a wonderful city with the normal security concerns that come with large cities. The Mexico City government has some tips here.
+
+// Mexico City is at high altitude (~7,350 feet), so it is important to remain hydrated. We suggest drinking bottled water and other beverages.
+
+// In August, the average daytime temperature is in the upper 70˚s and the evenings are in the 50˚s."
+
 export const TravelInformation = () => {
 	return (
 		<div className="wp-content">
@@ -5,8 +24,15 @@ export const TravelInformation = () => {
 			<h2>Passport</h2>
 			<p>
 				Mexico requires that your passport is valid upon entry to the
-				country. For additional information, please visit the U.S. State
-				Department's website.
+				country. For additional information, please visit the Mexican
+				government's{" "}
+				<a
+					target="_blank"
+					href="https://consulmex.sre.gob.mx/montreal/index.php/en/foreigners/visa/355-what-documents-do-i-need-to-enter-mexico"
+				>
+					website
+				</a>
+				.
 			</p>
 			<h2>Travel to Mexico City</h2>
 			<p>
@@ -37,7 +63,14 @@ export const TravelInformation = () => {
 			<p>
 				Mexico City is a wonderful city with the normal security
 				concerns that come with large cities. The Mexico City government
-				has some tips here.
+				has some tips{" "}
+				<a
+					href="https://mexicocity.cdmx.gob.mx/e/basics-for-mexico-city-travel/"
+					target="_blank"
+				>
+					here
+				</a>
+				.
 			</p>
 			<p>
 				Mexico City is at high altitude (~7,350 feet), so it is
