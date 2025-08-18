65bbd0b9e40a7e433e0db2ed165e39c0f5df1849
Michael Powers <mgrpowers@gmail.com>
2025-08-17 20:51:19 -0600

    removed sections

--- wagpow/src/App.tsx
+++ wagpow/src/App.tsx
@@ -20,68 +20,12 @@ const Nav = () => {
 			<a href="#home" className="[&.active]:font-bold">
 				J + M
 			</a>{" "}
-			<a href="#schedule" className="[&.active]:font-bold">
-				{isMobile ? "📅" : "Schedule"}
-			</a>
-			<a href="#travel" className="[&.active]:font-bold">
-				{isMobile ? "🛫" : "Travel"}
-			</a>
 			<a href="#things-to-do" className="[&.active]:font-bold">
 				{isMobile ? "🚶" : "Things To Do"}
 			</a>
 			<a href="#things-to-eat" className="[&.active]:font-bold">
 				{isMobile ? "🌮" : "Things To Eat"}
 			</a>
-		</div>
-	)
-}
-
-function MikeCheck() {
-	const [gamerTag, setGamerTag] = useState("")
-	const [isARealGamer, setIsARealGamer] = useState(false)
-
-	useEffect(() => {
-		if (gamerTag) {
-			const gamerTags = [
-				"Ton1c",
-				"C0lin",
-				"uh_ohFox",
-				"Clon1c",
-				"Oz",
-				"Kittenmittens",
-				"Hipperman",
-			]
-
-			if (
-				gamerTags
-					.map((tag) => tag.toLowerCase())
-					.includes(gamerTag.toLowerCase())
-			) {
-				setIsARealGamer(true)
-			}
-		}
-	}, [gamerTag])
-
-	return (
-		<div className="wp-content">
-			{isARealGamer ? (
-				<>
-					<h1>
-						Tell mike or jillian this is a nice party but you rather
-						be skateboarding.
-					</h1>
-				</>
-			) : (
-				<>
-					<h1>Wow you're a smartie!</h1>
-					<h2>This is for gamers only, please enter you gamer tag</h2>
-					<input
-						onChange={(e) => setGamerTag(e.target.value)}
-						type="text"
-						placeholder="Enter your gamer tag"
-					/>
-				</>
-			)}
 		</div>
 	)
 }
@@ -93,25 +37,11 @@ function App() {
 		return <Login />
 	}
 
-	if (window.location.pathname === "/oqvivlumhkit") {
-		return (
-			<Section id="home">
-				<MikeCheck />
-			</Section>
-		)
-	}
-
 	return (
 		<>
 			<Nav />
 			<Section id="home">
 				<Home />
-			</Section>
-			<Section id="schedule">
-				<Schedule />
-			</Section>
-			<Section id="travel">
-				<TravelInformation />
 			</Section>
 			<Section id="things-to-do">
 				<ThingsToDo />
